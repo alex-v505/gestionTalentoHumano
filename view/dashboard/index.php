6feb65a899060282ce1b7d941523503342687309
David202151 <davidalejandrovargas20@gmail.com>
2022-08-24 04:45:59 -0500

    Inicio añadido

--- view/dashboard/index.php
+++ view/dashboard/index.php
@@ -1,8 +1,33 @@
 <?php  include '../template/header.php'?>
     <!-- Main content -->
     <section class="content">
-        <h1>Hola mundo</h1>
-    </section>
+        <div class="row">
+    
+            <div class="col-12 px-5 mt-5 ">
+                <center>
+                <img src="../dist/img/ESPEtransparente (1).png" alt="" width="700" height="170">
+                </center>
+                <br>
+                <h1>¿Que es la gestion del talento humano?</h1>
+                    <h5>La gestión del talento humano hace referencia al conjunto de procesos que una organización, a través de su departamento de recursos humanos, pone en marcha para la atracción, captación e incorporación de nuevos colaboradores, al igual que para la retención de los que ya son parte de la empresa.</h5>
+                    <h5>Dada la importancia que representa para una entidad contar con colaboradores de excelencia, que muestran un firme compromiso con la empresa, la gestión del talento humano es un proceso hacia el que apuestan cada vez más organizaciones. Y es que, en realidad, no es para menos, porque una óptima gestión del talento humano puede ser un factor clave al buscar el aumento de la competitividad y la rentabilidad de cualquier empresa.</h5>
+                <h1>¿Cuál es la importancia de la gestión de talento humano en la empresa?</h1>
+                    <h5>Existen muchas ventajas para las empresas que realizan un esfuerzo orientado no solo en retener a su mejor talento, sino en fomentar su desempeño; y es que hay una fuerte relación entre tener un talento exitoso y obtener resultados empresariales óptimos.
+
+                        <br> Algunos de los beneficios que obtienen las compañías que gestionan el talento de sus trabajadores son:
+
+                        <br> - Menos pérdidas económicas ligadas a costes (despidos) y tiempos de formación de nuevos candidatos.
+                        <br> - Aumento de la productividad, con el consecuente mejoramiento económico para la empresa.
+                        <br> - Mejor clima laboral, fruto de la disminución de la rotación de personal.
+                        <br> - Mayor satisfacción laboral, que se traduce en una mejor exposición de la empresa (por medio de sus recomendaciones) y un mayor esfuerzo del empleado por obtener mejores resultados en su puesto de trabajo.
+                        <br> - Incremento de la lealtad, al saber que la finalidad del programa de gestión del talento humano los lleva a poder dar sus próximos pasos laborales dentro de la empresa.
+                        </h5>
+            
+            
+            
+            </div>
+        </div>
+        </section>
     <!-- /.content -->
     <?php  include '../template/footer.php'?>
     
